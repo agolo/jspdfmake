1ccbef41872cd28395f058ed92f9f144c3fb703b
Abkreno <mohamedelhamamsy789@gmail.com>
2018-10-23 18:45:04 +0200

    Add express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(`${__dirname}/build`));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/index.html'));
+});
+
+app.listen(port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
 const DashboardPlugin = require('webpack-dashboard/plugin');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const ENV = process.env.APP_ENV;
-const isTest = ENV === 'test';
-const isProd = ENV === 'production';
-
-// function to set dev-tool depending on environment
-function setDevTool() {
-  if (isTest) {
-    return 'inline-source-map';
-  }
-  if (isProd) {
-    return 'source-map';
-  }
-  return 'eval-source-map';
-}
-
-function setEntryPoint() {
-  if (isProd) {
-    return path.join(__dirname, '/src/index.js');
-  }
-  return path.join(__dirname, '/playground/test.js');
-}
 
 const config = {
-  entry: setEntryPoint(), // webpack entry point. Module to start building dependency graph
-  mode: ENV,
+  entry: path.join(__dirname, '/playground/test.js'), // webpack entry point. Module to start building dependency graph
+  mode: 'development',
+  devtool: 'eval-source-map', // Set the devtool
   output: {
-    path: path.join(__dirname, '/dist'), // Folder to store generated bundle
-    filename: 'jspdfmake.min.js', // Name of generated bundle after build
+    path: path.join(__dirname, '/build'), // Folder to store generated bundle
+    filename: 'bundle.js', // Name of generated bundle after build
     publicPath: '/', // public URL of the output directory when referenced in a browser
   },
-  devtool: setDevTool(), // Set the devtool
   module: { // where we defined file patterns and their loaders
     rules: [
       {
@@ -48,27 +24,15 @@ const config = {
   },
   plugins: [ // Array of plugins to apply to build chunk
     new DashboardPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/playground/public/index.html'),
+      inject: 'body',
+    }),
   ],
   devServer: { // configuration for webpack-dev-server
     contentBase: './playground/public', // source of static assets
     port: 7700, // port to run dev-server
   },
 };
-
-// Minify and copy assets in production
-if (isProd) {
-  // plugins to use in a production environment
-  config.plugins.push(
-    new UglifyJSPlugin(), // minify the chunk
-  );
-} else {
-  // plugins to use in a development/test environment
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/playground/public/index.html'),
-      inject: 'body',
-    }),
-  );
-}
 
 module.exports = config;
