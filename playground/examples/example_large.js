af5052e5f08355cb3d7fb7037676a773033762ac
Abkreno <mohamedelhamamsy789@gmail.com>
2018-11-08 14:00:26 +0200

    Add large example

---
+++ playground/examples/example_large.js
@@ -0,0 +1,12 @@
+const example = {
+  content: Array(2000).fill().map(() => ({
+    text: `
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni possimus aut consequuntur ab. Iusto voluptatem illum magnam quis fugiat, deserunt totam sit atque molestias necessitatibus perferendis, error delectus, laudantium amet.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni possimus aut consequuntur ab. Iusto voluptatem illum magnam quis fugiat, deserunt totam sit atque molestias necessitatibus perferendis, error delectus, laudantium amet.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni possimus aut consequuntur ab. Iusto voluptatem illum magnam quis fugiat, deserunt totam sit atque molestias necessitatibus perferendis, error delectus, laudantium amet.
+      `,
+    align: 'center',
+  }))
+};
+
+export default example;
